0b730e7375d450ad4ef46efb1df3b13ca4740548
bijithkk <informbijith@gmail.com>
2025-06-17 00:42:12 +0530

    updated

--- src/services/blogService.js
+++ src/services/blogService.js
@@ -1,6 +1,5 @@
 import axios from "axios";
-
-const API_BASE_URL = "http://localhost:3003/api";
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 export const getAllBlogs = (search = "") =>
   axios.get(`${API_BASE_URL}/blog/get`, {
